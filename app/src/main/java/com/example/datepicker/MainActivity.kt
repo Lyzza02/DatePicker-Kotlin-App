5db2c7107a06f67aa460fe21c25a7ead7ed16796
Elyzza Jane Abrigo <elyzza.abrigo@gmail.com>
2022-04-12 23:18:23 +0800

    [MOD] simple date picker w/o database

--- app/src/main/java/com/example/datepicker/MainActivity.kt
+++ app/src/main/java/com/example/datepicker/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.datepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CalendarView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calender = findViewById<View>(R.id.calendarView1) as CalendarView
+        val textview = findViewById<View>(R.id.textView2) as TextView
+
+        calender.setOnDateChangeListener { view, year, month, day ->
+            textview.text = "Date is : " + (month + 1) + " / " + day + " / " + year
+        }
     }
 }
